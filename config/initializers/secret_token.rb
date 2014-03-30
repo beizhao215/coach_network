eeba0c3a9c98d6cb4ecbffa6d9861ac3c585b96b
Bei Zhao <bei.zhao215@gmail.com>
2014-03-30 16:00:16 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoachNetwork::Application.config.secret_key_base = '955cff105a10defbaab701e18143eb9c6d049c0affd0ed844fa305c4fd2db03a6b77a6e4b803860702befde233ac64dfb2f673093957150d07bdbf383cd427b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoachNetwork::Application.config.session_store :cookie_store, key: '_coach_network_session'
