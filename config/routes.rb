04e45b2ce01cf431dadab7a38abf0b498ae2535a
Bei Zhao <bei.zhao215@gmail.com>
2014-03-30 19:27:43 -0500

    Update router for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 CoachNetwork::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/about"
+  match '/about', to:'static_pages#about', via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
     it "should be a valid page have content 'homepage'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('homepage')
     end
   end
   
   describe "About page" do
       it "should be a valid page have content 'about'" do
-        visit '/static_pages/about'
+        visit about_path
         expect(page).to have_content('about')
       end
     end
