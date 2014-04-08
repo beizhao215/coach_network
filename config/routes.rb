3360f68ee4a76d7f622607115c171e310479a068
Bei Zhao <bei.zhao215@gmail.com>
2014-04-08 18:01:10 -0500

    Basic student model(including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 CoachNetwork::Application.routes.draw do
+  get "students/new"
   resources :coaches
   resources :sessions, only: [:new, :create, :destroy]
   
   match '/about', to:'static_pages#about', via: 'get'
   match '/coach_signup', to: 'coaches#new', via: 'get'
+  match '/signup',  to: 'students#new',    via: 'get'
+  
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140406205425) do
+ActiveRecord::Schema.define(version: 20140408223057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,5 +33,15 @@ ActiveRecord::Schema.define(version: 20140406205425) do
 
   add_index "coaches", ["email"], name: "index_coaches_on_email", unique: true, using: :btree
   add_index "coaches", ["remember_token"], name: "index_coaches_on_remember_token", using: :btree
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+  end
+
+  add_index "students", ["email"], name: "index_students_on_email", unique: true, using: :btree
 
 end
