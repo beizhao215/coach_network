da88180b17b748a44452a07ecb598ea6bcaae686
Bei Zhao <bei.zhao215@gmail.com>
2014-04-14 15:26:33 -0500

    Finish group edit and delete

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
   before_action :signed_in_user, only: [:new, :create, :destroy]
+  before_action :correct_coach,   only: [:edit, :update, :destroy]
+  
 
   def index
   end
@@ -20,16 +22,38 @@ class GroupsController < ApplicationController
   
   def show
     @group = Group.find(params[:id])
+  end
+  
+  def edit
+    @group = Group.find(params[:id])
     
+  end
+  
+  def update
+    @group = Group.find(params[:id])
+    if @group.update_attributes(group_params)
+      flash[:success] = "Group updated"
+      redirect_to @group    
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    Group.find(params[:id]).destroy
+    flash[:success] = "Group destroyed"
+    redirect_to coach_path(current_coach)
   end
   
   private
   
       def group_params
         params.require(:group).permit(:name,:description)
+      end
+      
+      def correct_coach
+        @group = Group.find(params[:id])
+        redirect_to(root_path) unless current_coach.id == @group.coach_id
       end
       
 end
--- spec/requests/group_pages_spec.rb
+++ spec/requests/group_pages_spec.rb
@@ -34,5 +34,77 @@ describe "GroupPages" do
       end
     end
   end
+  
+  describe "edit" do
+    
+    let(:group) { FactoryGirl.create(:group, coach: coach, name:"test", description: "desc")}
+    
+    before do
+      visit edit_group_path(group)
+    end
+    
+
+    describe "page" do
+      it { should have_content("Update group information") }
+    end
+
+    describe "with invalid information" do
+      before do
+        fill_in "Name", with: ""
+        click_button "Save changes" 
+      end
+      it { should have_content('error') }
+    end
+    
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "valid name"
+        fill_in "Description", with: "valid description"
+        click_button "Save changes" 
+      end
+      it { should have_content('Group updated') }
+    end
+    
+    describe "cannot edit other's group" do
+      let(:coach2) { FactoryGirl.create(:coach) }
+      before do
+        sign_in coach2
+        visit edit_group_path(group)
+      end
+      it { should_not have_content("Update group information") }
+    end
+  end
+  
+  describe "show" do
+    let(:group) { FactoryGirl.create(:group, coach: coach, name:"test", description: "desc")}
+    
+    describe "should have group name, description, edit, delete" do
+      before { visit group_path(group) }
+      it { should have_content(group.name) }
+      it { should have_content(group.description) }
+      it { should have_link('edit', href: edit_group_path(group)) }
+      it { should have_link('delete', href: group_path(group)) }
       
+    end
+    
+    describe "should not see edit/delete link of other coach's group" do
+      let(:coach2) { FactoryGirl.create(:coach) }
+      before do
+        sign_in coach2
+        visit group_path(group)
+      end
+      it { should_not have_link("edit") }
+      it { should_not have_link("delete") }
+    end
+  end
+  
+  describe "delete" do
+    let(:group) { FactoryGirl.create(:group, coach: coach, name:"test", description: "desc")}
+    before { visit group_path(group) }
+    it "should be able to delete a group" do
+      expect do
+        click_link('delete')
+          end.to change(coach.groups, :count).by(-1)
+    end
+  end
 end
